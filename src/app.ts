ad895c16c29cbb653ca0323e774574e762c1d32a
Shajeeb <liveclasswd@gmail.com>
2023-07-19 10:27:50 +0600

    add increment system for user

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,21 @@
 import express, { Application, Request, Response } from 'express';
 const app: Application = express();
 import cors from 'cors';
+import router from './app/modules/users/users.route';
+// import usersService from './app/modules/users/users.service';
 
 // use cors
 app.use(cors());
+
+// application route
+app.use('/api/v1/users/', router);
 
 // parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // testing purpose
-app.get('/', (req: Request, res: Response) => {
+app.get('/', async (req: Request, res: Response) => {
   res.send('Ato pagol keno ami karo jonno');
 });
 
--- src/app/modules/users/users.service.ts
+++ src/app/modules/users/users.service.ts
@@ -1,11 +1,17 @@
+import config from '../../../config/index';
+import { geenrateUser } from './user.utils';
 import { IUser } from './users.interface';
 import { User } from './users.model';
 
 const createUser = async (user: IUser): Promise<IUser | null> => {
-  const createdUser = await User.create(user);
-
   //   autogeneraetd incremetn id
+  const id = await geenrateUser();
+  user.id = id;
   //   default password
+  if (!user.password) {
+    user.password = config.default_user_pass as string;
+  }
+  const createdUser = await User.create(user);
 
   if (!createUser) {
     throw new Error('failed to create user');
