f76eb86fda1009657294c74aec2f20d9f1a472c7
Shajeeb <liveclasswd@gmail.com>
2023-07-20 15:20:11 +0600

    [DONE] set looger using winstone

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,18 @@
 import mongoose from 'mongoose';
 import app from './app';
 import config from './config';
+import { errorLogger, logger } from './shared/looger';
 
 async function bootstrap() {
   try {
     await mongoose.connect(config.database_url as string);
-    console.log('Database connected successfully');
+    logger.info('Database connected successfully');
 
     app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`);
+      logger.info(`Example app listening on port ${config.port}`);
     });
   } catch (err) {
-    console.log('Failed to connect database');
+    errorLogger.error('Failed to connect database');
   }
 }
 
---
+++ src/shared/looger.ts
@@ -0,0 +1,29 @@
+import winston from 'winston';
+import path from 'path';
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({
+      filename: path.join(process.cwd(), 'logs', 'winstone', 'success.log'),
+      level: 'info',
+    }),
+  ],
+});
+const errorLogger = winston.createLogger({
+  level: 'error',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({
+      filename: path.join(process.cwd(), 'logs', 'winstone', 'error.log'),
+      level: 'error',
+    }),
+  ],
+});
+
+export { logger, errorLogger };
